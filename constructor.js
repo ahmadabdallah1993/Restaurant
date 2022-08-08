d17ccdb483e2c7a1d620d9cea259a91d01a06c15
Ahmad Yaghi <ahmad.abdallah1993@outlook.com>
2022-08-08 20:49:10 +0300

    2nd done

--- constructor.js
+++ constructor.js
@@ -20,8 +20,38 @@ Foods.prototype.foodID = function () {
 }
 
 
+const bodyEl = document.getElementsByTagName('body');
 
 
+let formEl = document.getElementById('form');
+
+formEl.addEventListener('submit', handleSubmit);
+
+function handleSubmit(event) {
+    event.preventDefault();
+    console.log(event);
+
+    let name = event.target.name.value;
+    let selectt = event.target.select.value;
+    let price = event.target.price.value;
+
+    //console.log(name,selectt, price);
+
+    if(selectt == 1) {
+        selectt = "Fruit and Vegetables";
+    }else if(selectt == 2) {
+        selectt = "Starchy Food";
+    }else if(selectt == 3) {
+        selectt = "Diary";
+    }else if(selectt == 4) {
+        selectt = "Protein";
+    }else if(selectt == 5) {
+        selectt = "Fat";
+    }
 
 
+    //console.log(name,selectt, price);
 
+    
+
+}
